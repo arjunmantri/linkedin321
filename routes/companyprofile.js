c80e5984b25c094d89f305448a460fca68671cf2
yashoswal <yashoswal@users.noreply.github.com>
2015-04-12 16:12:01 -0700

    search function added to company profile

--- routes/companyprofile.js
+++ routes/companyprofile.js
@@ -1,4 +1,5 @@
 CompanyProfile = require('../models/CompanyModel');
+JobPosts = require('../models/JobPostsModel');
 
 
 exports.getCompanyProfile = function(req, res){
@@ -51,20 +52,7 @@ exports.updateCompanyInfo = function(req, res){
 }
 
 
-/*var config = require('../models/auth');
-var ejs = require("ejs");
-var ddb = require('dynamodb').ddb({ accessKeyId:config.ACCESS_KEY,secretAccessKey: config.ACCESS_SECRET, endpoint:config.ENDPOINT});
 
-//Connecting to the memcached using AWS sdk.
-var AWS = require('aws-sdk'),
-    Memcached = require('memcached');
-var mc = Memcached.Client.create('hostname:port', {
-  username: 'username',
-  password: 'password'
-});
-
-
-JobPosts = require('../models/JobPostsModel');
 
 exports.getEditProfile = function(req,res){
 	ejs.renderFile('./views/companyeditprofile.ejs',function(err, result) {
@@ -98,28 +86,6 @@ exports.jobPosts = function(req,res){
 			throw err;
 		console.log("job post added : " + jp);
 	});
-
-	//Saving in Memcahed
-	var locationForJob = req.body.JobLocation;
-	var SkillForJob	= req.body.SkillSet;
-	var JobNameForJob = req.body.JobName;
-	mc.set(locationForJob, JobPosts,1000000,function(err,result){
-	if(err) console.error(err);
-	
-	console.dir(result);
-	mc.end();
-});
-	mc.set(SkillForJob,JobPosts,1000000,function(err,result){
-	if(err) console.error(err);
-	console.dir(result);
-	mc.end();
-});
-	mc.set(JobNameForJob,JobPosts,1000000,function( err, result ){
-	if(err) console.error(err);
-	console.dir(result);
-	mc.end();
-});
-	//End of saving to memcached
 
 	CompanyProfile.findOne({'CompanyId':req.body.CompanyId},function(err,response){
 		if (err)
@@ -155,51 +121,14 @@ exports.getJobPosts = function(req,res){
 			throw err;
 		console.log("job post added : " + jp);
 	});
-
-	//Saving in Memcahed
-	//var locationForJob = //from the client side//
-	//var SkillForJob	= //from the client side//
-	//var JobNameForJob = //form the client side//
-	mc.get(locationForJob,function(err,response,key){
-		if(response != null){
-
-		}
-	});
-	mc.get(SkillForJob,function(err,response,key){
-		if(response != null){
-
-		}
-	});
-	mc.get(JobNameForJob,function(err,response,key){
-		if(response != null){
-
-		}
-	});
-	//
-	// CompanyProfile.findOne({'CompanyId':req.body.CompanyId},function(err,response){
-	// 	if (err)
-	// 		throw err;
-	// 	response.JobPosts.push(h);
-	// 	response.save(function(err){
-	// 		if(err)
-	// 			throw err;
-	// 		console.log(response);
-	// 	});
-		
-	// });
-	ejs.renderFile('./views/jobposts.ejs',function(err, result) {
-		   // render on success
-		   if (!err) {
-		            res.end(result);
-		   }
-		   // render or error
-		   else {
-		            res.end('An error occurred');
-		            console.log(err);
-		   }
-	   });
 	
 };
 
-
-	*/
+//Get function for the Job Search:-
+exports.getJobPosts = function(req,res){
+	JobPosts.find({'SkillSet':req.params.key},function(err,response){
+		if(err)
+			throw err;
+		res.json(response);
+	})
+};
