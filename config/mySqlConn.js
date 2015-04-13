019dc4e98210cc8b651920623046e023901a41d0
Arjun Mantri <arjunmantri@users.noreply.github.com>
2015-04-12 20:26:24 -0700

    Update mySqlConn.js

--- config/mySqlConn.js
+++ config/mySqlConn.js
@@ -1,15 +1,26 @@
 var mysql_pool = require('mysql');
 
 /* MYSQL RDS */
-var pool  = mysql_pool.createPool({
-	host     : 'sampledb.cy0fkl5xnx4r.us-west-1.rds.amazonaws.com',
-	user     : 'root',
-	password : 'rootroot',
-	port     : '3306',
-	database : 'sampledb',
-	connectionLimit : '10'
-});
+// var pool  = mysql_pool.createPool({
+// 	host     : 'sampledb.cy0fkl5xnx4r.us-west-1.rds.amazonaws.com',
+// 	user     : 'root',
+// 	password : 'rootroot',
+// 	port     : '3306',
+// 	database : 'sampledb',
+// 	connectionLimit : '10'
+// });
 
+var mysql = require('mysql');
+var dbconfig = require('/srv/www/sampledb/shared/config/opsworks.js');
+
+var pool = mysql.createPool({
+  host: dbconfig.db['host'],
+  user: dbconfig.db['username'],
+  password: dbconfig.db['password'],
+  port: dbconfig.db['port'],
+  database: dbconfig.db['database'],
+  connectionLimit : '10'
+});
 
 /* LOCAL MYSQL CONNECTION*/
 /*var pool  = mysql_pool.createPool({
